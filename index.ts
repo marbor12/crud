c2109a8b63148b41799748f25b08c70151a20427
maria <mariaboro01@gmail.com>
2026-09-21 13:13:41 +0700

    Add basic Hono routes and GET /user

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono(); //buat pelayan kosong
+
+app.get("/", (c) => {
+    return c.text("CRUD Sederhana dengan Hono");
+});
+
+app.get("/halo", (c) => {
+    return c.text("Halo, Dunia!");
+});
+
+app.get("/user", (c) => {
+    return c.json({
+        id: 1,
+        name: "Maria S",
+        email: "mariaboro01@gmail.com"
+    })
+})
+
+export default {
+    port: 3000,
+    fetch: app.fetch,
+};
